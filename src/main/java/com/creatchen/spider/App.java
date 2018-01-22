705d486fdaf3fac2d89784f9b3ecfc2d59864930
creatchen <creatchen@lexinfintech.com>
2018-01-22 18:23:20 +0800

    add .gitignore

--- src/main/java/com/creatchen/spider/App.java
+++ src/main/java/com/creatchen/spider/App.java
@@ -1,5 +1,7 @@
 package com.creatchen.spider;
 
+import com.creatchen.spider.pageProcessor.GroupPageProcessor;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args ) {
         // 加载布隆过滤器
-
+        GroupPageProcessor.runSpider();
     }
 }
